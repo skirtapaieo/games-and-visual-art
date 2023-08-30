eb4e3e7147a97931538e1a72c87972161339c939
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-30 11:26:50 +0200

    added black edge and removed text

--- voxels/voxel.js
+++ voxels/voxel.js
@@ -3,26 +3,44 @@ import * as THREE from "three";
 // Initialize Scene and Camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  75, // Field of View
+  window.innerWidth / window.innerHeight, // Aspect Ratio
+  0.1, // Near Clipping Plane
+  1000 // Far Clipping Plane
 );
+
+// Position Camera
+camera.position.z = 5;
+
+// Initialize Renderer
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor("#ffffff", 1);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // Create Voxel (Cube)
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 
+// Create edges for voxel
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(
+  edges,
+  new THREE.LineBasicMaterial({ color: 0x000000 })
+);
+
 // Add Voxel to Scene
 scene.add(cube);
+scene.add(line);
 
 // Render Scene
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  line.rotation.x += 0.01;
+  line.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
